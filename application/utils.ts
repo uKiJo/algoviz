c557aa14ce8d62e3adcb88ba436c4b72ef7a1202
ukijo <nes-you-ukijo@hotmail.fr>
2023-04-19 15:27:47 +0100

    add generate array numbers function

---
+++ application/utils.ts
@@ -0,0 +1,16 @@
+function generateRandomArray(): number[] {
+  const length = 10;
+  const max = 50;
+  const min = 0;
+  const randomArray: number[] = [];
+
+  while (randomArray.length < length) {
+    const randomNumber: number =
+      Math.floor(Math.random() * (max - min + 1)) + min;
+    if (!randomArray.includes(randomNumber)) {
+      randomArray.push(randomNumber);
+    }
+  }
+
+  return randomArray;
+}
